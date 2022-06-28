c30513aa6e859a48f8158e99b8fd02079c680ba5
Jungsu Kim <jungsukim2609@gmail.com>
2022-06-28 14:31:39 -0600

    Finished

--- src/components/ProjectDetailsModal.js
+++ src/components/ProjectDetailsModal.js
@@ -36,7 +36,67 @@ class ProjectDetailsModal extends Component {
       }
     }
     return (
-      <Modal>
+      <Modal {...this.props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      className="modal-inside">
+      <span onClick={this.props.onHide} className="modal-close">
+      <i className="fas fa-times fa-3x close-icon"></i>
+    </span>
+    <div className="col-md-12">
+      <div className="col-md-10 mx-auto" style={{ paddingBottom: "50px" }}>
+        <div className="slider-tab">
+          <span
+            className="iconify slider-iconfiy"
+            data-icon="emojione:red-circle"
+            data-inline="false"
+            style={{ marginLeft: "5px" }}
+          ></span>{" "}
+          &nbsp;{" "}
+          <span
+            className="iconify slider-iconfiy"
+            data-icon="twemoji:yellow-circle"
+            data-inline="false"
+          ></span>{" "}
+          &nbsp;{" "}
+          <span
+            className="iconify slider-iconfiy"
+            data-icon="twemoji:green-circle"
+            data-inline="false"
+          ></span>
+        </div>
+        <AwesomeSlider
+          cssModule={[AwesomeSliderStyles, AwesomeSliderStyles2]}
+          animation="scaleOutAnimation"
+          className="slider-image"
+        >
+          {img}
+        </AwesomeSlider>
+      </div>
+      <div className="col-md-10 mx-auto">
+        <h3 style={{ padding: "5px 5px 0 5px" }}>
+          {title}
+          {url ? (
+            <a
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link-href"
+            >
+              <i
+                className="fas fa-external-link-alt"
+                style={{ marginLeft: "10px" }}
+              ></i>
+            </a>
+          ) : null}
+        </h3>
+        <p className="modal-description">{description}</p>
+        <div className="col-md-12 text-center">
+          <ul className="list-inline mx-auto">{tech}</ul>
+        </div>
+      </div>
+    </div>
       </Modal>
     );
   }
